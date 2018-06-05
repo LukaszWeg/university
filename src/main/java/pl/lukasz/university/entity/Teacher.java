d1c5219bb65601a05326b55cb08ea33263dba308
LukaszWeg <lukasz0596@gmail.com>
2018-06-05 10:42:28 +0200

    Add teacher entity

---
+++ src/main/java/pl/lukasz/university/entity/Teacher.java
@@ -0,0 +1,58 @@
+package pl.lukasz.university.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Teacher {
+
+    @Id
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private long contactNumber;
+    private String email;
+
+    public Teacher() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public long getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
