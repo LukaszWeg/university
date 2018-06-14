e25f6885ece1f5634fd0f5057b00a88a472134fe
LukaszWeg <lukasz0596@gmail.com>
2018-06-14 13:47:51 +0200

    set Presence

--- src/main/java/pl/lukasz/university/controller/teacher/TeacherSubjectsController.java
+++ src/main/java/pl/lukasz/university/controller/teacher/TeacherSubjectsController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.lukasz.university.entity.ConnectTable;
+import pl.lukasz.university.entity.Student;
+import pl.lukasz.university.entity.Subject;
+import pl.lukasz.university.repository.ConnectTableRepository;
 import pl.lukasz.university.service.StudentService;
 import pl.lukasz.university.service.SubjectService;
 
@@ -19,10 +22,12 @@ public class TeacherSubjectsController {
 
     private SubjectService subjectService;
     private StudentService studentService;
+    private ConnectTableRepository connectTableRepository;
 
-    public TeacherSubjectsController(SubjectService subjectService, StudentService studentService) {
+    public TeacherSubjectsController(SubjectService subjectService, StudentService studentService, ConnectTableRepository connectTableRepository) {
         this.subjectService = subjectService;
         this.studentService = studentService;
+        this.connectTableRepository = connectTableRepository;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -35,9 +40,33 @@ public class TeacherSubjectsController {
     }
 
     @RequestMapping(value = "/{subjectId}/{studentId}", method = RequestMethod.GET)
-    public String setPresence(Model model) {
-        model.addAttribute("newPresence", new ConnectTable());
+    public String setPresence(Model model,@PathVariable ("subjectId") Long subjectid, @PathVariable ("studentId") Long studentId) {
+        Subject subject = subjectService.findById(subjectid);
+        Student student = studentService.findById(studentId);
+        ConnectTable connectTable = connectTableRepository.findByStudentAndSubject(student, subject);
+        if (connectTable != null) {
+            model.addAttribute("newPresence", connectTable);
+        }
+        else
+        {
+            model.addAttribute("newPresence", new ConnectTable());
+        }
+
+
         return "teacher/setPresence";
+    }
+
+    @RequestMapping(value = "/{subjectId}/{studentId}", method = RequestMethod.POST)
+    public String savePresence(@PathVariable ("subjectId") Long subjectid, @PathVariable ("studentId") Long studentId, @ModelAttribute ("newPresencet") ConnectTable connectTable)
+    {
+        Subject subject = subjectService.findById(subjectid);
+        Student student = studentService.findById(studentId);
+        connectTable.setStudent(student);
+        connectTable.setSubject(subject);
+        connectTableRepository.save(connectTable);
+        return "redirect:/mysubjects/"+ subjectid;
+
+
     }
 
 
--- src/main/java/pl/lukasz/university/repository/ConnectTableRepository.java
+++ src/main/java/pl/lukasz/university/repository/ConnectTableRepository.java
@@ -3,7 +3,12 @@ package pl.lukasz.university.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.lukasz.university.entity.ConnectTable;
+import pl.lukasz.university.entity.Student;
+import pl.lukasz.university.entity.Subject;
 
 @Repository
 public interface ConnectTableRepository extends JpaRepository<ConnectTable, Long> {
+
+    ConnectTable findByStudentAndSubject(Student student, Subject subject);
+
 }
